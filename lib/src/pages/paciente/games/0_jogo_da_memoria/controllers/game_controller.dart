005548620229a3c407c6a001a978e7273d8741c1
Wemerson Damasceno <wemersondamasceno7@gmail.com>
2022-03-24 21:14:37 -0300

    Finalizando o jogo da memória

--- lib/src/pages/paciente/games/0_jogo_da_memoria/controllers/game_controller.dart
+++ lib/src/pages/paciente/games/0_jogo_da_memoria/controllers/game_controller.dart
@@ -67,4 +67,29 @@ class GameController {
   final int quantidadeCartas = 12;
 
   List<Map<int, CartaMemoriaModel>> cartasIguais = [];
+  int pontos = 0;
+
+  void virarCarta(int index) {
+    if (listaDeCartas[index].isEscondida) {
+      listaDeCartas[index].isEscondida = false;
+      cartasIguais.add(
+        {index: listaDeCartas[index]},
+      );
+    }
+    //se as cartas forem iguais
+    if (cartasIguais.length == 2) {
+      if (cartasIguais[0].values.first.valor ==
+          cartasIguais[1].values.first.valor) {
+        pontos += 100;
+        cartasIguais.clear();
+      }
+    } else {
+      //se não forem igual limpa
+      Future.delayed(const Duration(milliseconds: 500), () {
+        listaDeCartas[cartasIguais[0].keys.first].isEscondida = true;
+        listaDeCartas[cartasIguais[1].keys.first].isEscondida = true;
+        cartasIguais.clear();
+      });
+    }
+  }
 }
--- lib/src/pages/paciente/games/0_jogo_da_memoria/pages/jogo_da_memoria_page.dart
+++ lib/src/pages/paciente/games/0_jogo_da_memoria/pages/jogo_da_memoria_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:fonoplay/src/constants/constants_colors.dart';
 import 'package:fonoplay/src/pages/paciente/games/0_jogo_da_memoria/components/card_game_widget.dart';
 import 'package:fonoplay/src/pages/paciente/games/0_jogo_da_memoria/controllers/game_controller.dart';
+import 'package:fonoplay/src/pages/widgets/button_gradiente_widget.dart';
 import 'package:fonoplay/src/pages/widgets/cabecalho_widget.dart';
 import 'package:fonoplay/src/pages/widgets/container_gradiente_widget.dart';
+import 'package:lottie/lottie.dart';
 
 class JogoDaMemoriaPage extends StatefulWidget {
   const JogoDaMemoriaPage({Key? key}) : super(key: key);
@@ -14,12 +16,13 @@ class JogoDaMemoriaPage extends StatefulWidget {
 
 class _JogoDaMemoriaPageState extends State<JogoDaMemoriaPage> {
   int pontos = 0;
-  final game = GameController();
+  final gameController = GameController();
+  bool exibirAnimacao = false;
 
   @override
   void initState() {
     super.initState();
-    game.listaDeCartas.shuffle();
+    gameController.listaDeCartas.shuffle();
   }
 
   @override
@@ -49,83 +52,151 @@ class _JogoDaMemoriaPageState extends State<JogoDaMemoriaPage> {
             top: size.height * 0.25,
             left: 0,
             right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 10),
-                  child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        "Pontos: $pontos",
-                        style: const TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w800,
-                          color: ConstantColor.titlesColor,
-                        ),
-                      )),
-                ),
-                SizedBox(
-                  height: size.height * .7,
-                  width: size.width * 0.9,
-                  child: GridView.builder(
-                    itemCount: game.quantidadeCartas,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 5,
-                      mainAxisSpacing: 3,
-                    ),
-                    itemBuilder: (context, index) {
-                      var item = game.listaDeCartas[index];
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            if (game.listaDeCartas[index].isEscondida) {
-                              game.listaDeCartas[index].isEscondida = false;
-                              game.cartasIguais.add(
-                                {index: game.listaDeCartas[index]},
-                              );
-                            }
-                          });
-                          //se as cartas forem iguais
-                          if (game.cartasIguais.length == 2) {
-                            if (game.cartasIguais[0].values.first.valor ==
-                                game.cartasIguais[1].values.first.valor) {
-                              pontos += 100;
-                              game.cartasIguais.clear();
-                            } else {
-                              //se não forem igual limpa
-                              Future.delayed(const Duration(milliseconds: 500),
-                                  () {
-                                setState(() {
-                                  game
-                                      .listaDeCartas[
-                                          game.cartasIguais[0].keys.first]
-                                      .isEscondida = true;
-                                  game
-                                      .listaDeCartas[
-                                          game.cartasIguais[1].keys.first]
-                                      .isEscondida = true;
-                                  game.cartasIguais.clear();
-                                });
-                              });
-                            }
-                          }
-                        },
-                        child: CardGameWidget(
-                          isEscondida: item.isEscondida,
-                          pathImage: game.listaDeCartas[index].pathImage,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
+            child: listaDasCartas(size),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget listaDasCartas(Size size) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 10),
+          child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Pontos: $pontos",
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w800,
+                  color: ConstantColor.titlesColor,
+                ),
+              )),
+        ),
+        SizedBox(
+          height: size.height * .7,
+          width: size.width * 0.9,
+          child: GridView.builder(
+            itemCount: gameController.quantidadeCartas,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 3,
+            ),
+            itemBuilder: (context, index) {
+              var item = gameController.listaDeCartas[index];
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    if (gameController.listaDeCartas[index].isEscondida) {
+                      gameController.listaDeCartas[index].isEscondida = false;
+                      gameController.cartasIguais.add(
+                        {index: gameController.listaDeCartas[index]},
+                      );
+                    }
+                  });
+                  //se as cartas forem iguais
+                  if (gameController.cartasIguais.length == 2) {
+                    if (gameController.cartasIguais[0].values.first.valor ==
+                        gameController.cartasIguais[1].values.first.valor) {
+                      pontos += 100;
+                      gameController.cartasIguais.clear();
+
+                      //Se atingir os 600 pontos, acabou o jogo
+                      if (pontos == 600) {
+                        setState(() {
+                          exibirAnimacao = true;
+                          dialogComemoracao(size);
+                        });
+                      }
+                    } else {
+                      //Se não forem igual limpa
+                      Future.delayed(const Duration(milliseconds: 500), () {
+                        setState(() {
+                          gameController
+                              .listaDeCartas[
+                                  gameController.cartasIguais[0].keys.first]
+                              .isEscondida = true;
+                          gameController
+                              .listaDeCartas[
+                                  gameController.cartasIguais[1].keys.first]
+                              .isEscondida = true;
+                          gameController.cartasIguais.clear();
+                        });
+                      });
+                    }
+                  }
+                },
+                child: CardGameWidget(
+                  isEscondida: item.isEscondida,
+                  pathImage: gameController.listaDeCartas[index].pathImage,
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  dialogComemoracao(size) {
+    return showDialog<String>(
+      context: context,
+      builder: (_) => AlertDialog(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(14))),
+        elevation: 0,
+        content: SizedBox(
+          width: double.infinity,
+          height: size.height / 1.8,
+          child: Column(
+            children: [
+              LottieBuilder.asset(
+                "assets/images/animations/estrela_oculos_animacao.json",
+                width: size.width * 0.48,
+              ),
+              const Text(
+                "Parabéns!",
+                style: TextStyle(
+                  color: Color(0xFFebc600),
+                  fontSize: 30,
+                  fontWeight: FontWeight.w800,
+                  letterSpacing: 4,
+                ),
+              ),
+              const Text(
+                "Seu exercício foi concluido!",
+                style: TextStyle(
+                  color: Color(0xFFebc600),
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ButtonGradienteWidget(
+                texto: "Continuar",
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                habilitarBotao: true,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Jogar novamente",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
